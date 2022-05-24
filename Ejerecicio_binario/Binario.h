1f0a51d52395989b8827a09c85444baa3557fc5e
Juan Francisco Reyes Salazar <105328338+JuanFReyes4@users.noreply.github.com>
2022-05-23 20:26:33 -0500

    Update Binario.h

--- Ejerecicio_binario/Binario.h
+++ Ejerecicio_binario/Binario.h
@@ -1,3 +1,15 @@
+
+/*	UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+	DEPARTAMENTO DE CIENCIAS DE LA COMPUTACION
+    INGENIERA DE SOFTWARE
+                
+	AUTORES:    #Grupo 7
+				Brandon Raul Masacela - 	 brmasacela@espe.edu.ec
+				Juan Francisco Reyes -       jfreyes4@espe.edu.ec
+	FECHA DE CREACION:        20-05-2022 	
+	FECHA DE MODIFICACION:    23-05-2022
+*/
+
 #include <iostream>
 #include <cstring>
 
