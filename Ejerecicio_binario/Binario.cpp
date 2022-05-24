c8ef7e940afe0f423a241bb3e898d2bfb0694ec3
Juan Francisco Reyes Salazar <105328338+JuanFReyes4@users.noreply.github.com>
2022-05-23 20:27:02 -0500

    Update Binario.cpp

--- Ejerecicio_binario/Binario.cpp
+++ Ejerecicio_binario/Binario.cpp
@@ -1,3 +1,14 @@
+/*	UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+	DEPARTAMENTO DE CIENCIAS DE LA COMPUTACION
+    INGENIERA DE SOFTWARE
+                
+	AUTORES:    #Grupo 7
+				Brandon Raul Masacela - 	 brmasacela@espe.edu.ec
+				Juan Francisco Reyes -       jfreyes4@espe.edu.ec
+	FECHA DE CREACION:        20-05-2022 	
+	FECHA DE MODIFICACION:    23-05-2022
+*/
+
 #include "Binario.h"
 
 void Binario::decimalBinario(int val, char *cad){
